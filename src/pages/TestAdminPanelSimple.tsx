847ac8292903f3332149e3452a5ca08795cf5336
MOBIN (Work) <MOBIN (Work)@MOBIN>
2025-07-21 18:23:25 -0400

    Done

--- src/pages/TestAdminPanelSimple.tsx
+++ src/pages/TestAdminPanelSimple.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import RealTimeAdminDashboard from '@/components/RealTimeAdminDashboard';
 
 // Mock window.ezsite.apis for testing
